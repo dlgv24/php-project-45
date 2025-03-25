3f87a38b8cbbbda07c6482c4d867b4d9f04ca904
Alexander Dolgov <dlgv24@yandex.ru>
2025-03-25 16:58:50 +0300

    update greet() func

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,10 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greet(): void
+function greet(): string
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return $name;
 }
